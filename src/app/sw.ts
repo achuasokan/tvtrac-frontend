dce38bba198fb4d7cbb2de01b290a9f0fe77e21b
achuasokan <achuasokan555@gmail.com>
2026-08-11 02:10:10 +0530

    addeed cache for iamge config in the sw

--- src/app/sw.ts
+++ src/app/sw.ts
@@ -2,7 +2,7 @@
 
 import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
-import { Serwist, CacheFirst, NetworkFirst, StaleWhileRevalidate } from "serwist";
+import { Serwist, CacheFirst, NetworkFirst, StaleWhileRevalidate, CacheableResponsePlugin, ExpirationPlugin } from "serwist";
 
 declare global {
   interface WorkerGlobalScope extends SerwistGlobalConfig {
@@ -46,12 +46,30 @@ const serwist = new Serwist({
       matcher: /^https:\/\/image\.tmdb\.org\/.*/i,
       handler: new CacheFirst({
         cacheName: "tmdb-images",
+        plugins: [
+          new CacheableResponsePlugin({
+            statuses: [0, 200],
+          }),
+          new ExpirationPlugin({
+            maxEntries: 500,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+          }),
+        ],
       }),
     },
     {
       matcher: /^https:\/\/res\.cloudinary\.com\/.*/i,
       handler: new CacheFirst({
         cacheName: "cloudinary-images",
+        plugins: [
+          new CacheableResponsePlugin({
+            statuses: [0, 200],
+          }),
+          new ExpirationPlugin({
+            maxEntries: 100,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+          }),
+        ],
       }),
     },
     {
